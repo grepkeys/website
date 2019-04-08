ed813cde8da9a7ac3f66af8538066d1ff4023f48
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-04-08 19:26:04 +1000

    [deploy] Check if unstaged changes exist before stashing

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
+# Get us into the root of the project
+cd $(git rev-parse --show-toplevel)
 origpwd=$(pwd)
 
-# We stash to make sure the version of the website committed to
-# grepkeys.github.io matches what the website looked like as of the last commit.
-git stash
+# We stash (but only if there are unstaged changes) to make sure the version of
+# the website committed to grepkeys.github.io matches what the website looked
+# like as of the last commit.
+if ! git diff-index --quiet HEAD --; then
+  git stash --include-untracked --keep-index
+fi
+
 rm -r public/*
 hugo > /dev/null
-git stash pop
+
+if ! git diff-index --quiet HEAD --; then
+  git stash pop
+fi
 
 cd public
+git pull > /dev/null
 git add . > /dev/null
 
 msg="Rebuilding site $(date)"
