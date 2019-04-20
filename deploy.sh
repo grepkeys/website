2a3cef2db5f7c947ea3f2c916d5d011d34245342
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-04-20 17:45:45 +1000

    [deploy] Switch to master before running Hugo

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,15 @@ if ! git diff-index --quiet HEAD --; then
   git stash --include-untracked --keep-index
 fi
 
-# I never run `hugo` by itself during normal development -- only `hugo server`.
-# So the only reason the public folder would be different to GitHub is if others
-# have made changes. To avoid merge conflicts, pull in everyone else's changes
-# before running Hugo.
-cd public && git pull > /dev/null && cd $projroot
+# I never run `hugo` by itself during normal development -- only `hugo server`,
+# so the only reason the public folder would be different to GitHub is if others
+# have made changes. Pull in these changes before running Hugo to avoid merge
+# conflicts.
+cd public
+# Switch to master in case branches have been switched
+git checkout master
+git pull > /dev/null
+cd $projroot
 rm -r public/*
 hugo --minify > /dev/null
 
