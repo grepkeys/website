457a5ae003c194c50a766ab5538632684e6866d1
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-05-07 20:34:29 +1000

    [deploy] Wipe history of public/ on each run

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,8 @@ if ! git diff-index --quiet HEAD --; then
   git stash --include-untracked --keep-index > /dev/null
 fi
 
-# I never run `hugo` by itself during normal development -- only `hugo server`,
-# so the only reason the public folder would be different to GitHub is if others
-# have made changes. Pull in these changes before running Hugo to avoid merge
-# conflicts.
-cd public
-# Switch to master in case branches have been switched
-git checkout master
-git pull > /dev/null
-cd $projroot
-rm -r public/*
+# Clear out public/ before generating its contents again with Hugo
+rm -rf "public/*"
 hugo --minify > /dev/null
 
 # Restore any unstaged changed that were removed by the git stash earlier
@@ -28,10 +20,17 @@ if ! git diff-index --quiet HEAD --; then
   git stash pop > /dev/null
 fi
 
-cd public
+# Create a fresh branch, 'tmp', in public/ to which the compiled website is
+# committed. Then delete the master branch and rename tmp to master. This
+# effectively clears the history of the repo. Now that the history has been
+# cleared we can force push the repo to GitHub.
+#
+# We do this because the history of the public/ folder is worthless to us, so
+# there's no reason to keep it.
+cd "public"
+git checkout --orphan tmp > /dev/null
 git add . > /dev/null
-
-msg="Rebuilding site $(date)"
-git commit -m "$msg" > /dev/null
-
-git push origin master > /dev/null
+git commit --message "Rebuilding site $(date)" > /dev/null
+git branch --delete --force master > /dev/null
+git branch --move master > /dev/null
+git push --force origin master > /dev/null
