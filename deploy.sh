20b96a8ba187516eba033f2f771efecbba4de995
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-05-25 09:52:34 +1000

    [deploy] Fix bug where multiple fingerprinted files were kept

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,12 @@ if ! git diff-index --quiet HEAD --; then
 fi
 
 # Clear out public/ before generating its contents again with Hugo
+#
+# For some reason it helps to empty out the directories that have fingerprinted
+# content first, but I am not sure why _at all_. How is that meant to help at
+# all?! But it does prevent having multiple versions of a fingerprinted file,
+# only one of which is actually being used.
+rm "public/"{"scss","js"}/*
 rm -rf "public/*"
 hugo --minify > /dev/null
 
