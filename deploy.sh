dc3317865e625741bfaeffc642862d887b42570d
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-06-20 20:26:13 +1000

    [deploy] Keep compiled version's history

--- deploy.sh
+++ deploy.sh
@@ -26,17 +26,7 @@ if ! git diff-index --quiet HEAD --; then
   git stash pop > /dev/null
 fi
 
-# Create a fresh branch, 'tmp', in public/ to which the compiled website is
-# committed. Then delete the master branch and rename tmp to master. This
-# effectively clears the history of the repo. Now that the history has been
-# cleared we can force push the repo to GitHub.
-#
-# We do this because the history of the public/ folder is worthless to us, so
-# there's no reason to keep it.
 cd "public"
-git checkout --orphan tmp > /dev/null
 git add . > /dev/null
 git commit --message "Rebuilding site $(date)" > /dev/null
-git branch --delete --force master > /dev/null
-git branch --move master > /dev/null
 git push --force origin master > /dev/null
